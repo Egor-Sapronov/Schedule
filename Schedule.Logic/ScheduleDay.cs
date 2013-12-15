ca53417752367334a667ba53f806324eff4f46a5
egor <Sapronov.egor@gmail.com>
2013-12-15 13:24:00 +0700

    Add schedule day model

---
+++ Schedule.Logic/ScheduleDay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule.Logic
+{
+    public class ScheduleDay
+    {
+        private ScheduleShift _first = new ScheduleShift();
+        private ScheduleShift _second = new ScheduleShift();
+        private ScheduleShift _third = new ScheduleShift();
+
+        public ScheduleShift First { get { return _first; } }
+        public ScheduleShift Second { get { return _second; } }
+        public ScheduleShift Third { get { return _third; } }
+
+        public void Copy(ScheduleDay scheduleDay)
+        {
+            First.Copy(scheduleDay.First);
+            Second.Copy(scheduleDay.Second);
+            Third.Copy(scheduleDay.Third);
+        }
+    }
+}
