88ff494973a202bce70f7d3a08918dbccef1b7d2
egor <Sapronov.egor@gmail.com>
2013-12-15 14:45:23 +0700

    Add app main vm

---
+++ Schedule/AppState.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.ComponentModel;
+using Schedule.Logic;
+
+namespace Schedule.Logic.ViewModel
+{
+    public class AppState : INotifyPropertyChanged
+    {
+        private App _application;
+        private Scheduler _scheduler;
+        private Schedule _schedule;
+
+        internal AppState(App application)
+        {
+            _application = application;
+            _scheduler = new Scheduler();
+            _schedule = null;
+        }
+
+        public App Application { get { return _application; } }
+        public Scheduler Scheduler { get { return _scheduler; } }
+        public Schedule Schedule 
+        { 
+            get { return _schedule; }
+            set
+            {
+                if (value != _schedule)
+                {
+                    _schedule = value;
+                    RaisePropertyChanged(new PropertyChangedEventArgs("Schedule"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, e);
+            }
+        }
+    }
+}
