30cd5c52c7970ae4f421cd3bb24c5355a18a7c3f
egor <Sapronov.egor@gmail.com>
2013-12-15 15:04:50 +0700

    Add wpf controls for schedule

---
+++ Schedule/Controls/ScheduleControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Schedule.Controls
+{
+    /// <summary>
+    /// Interaction logic for ScheduleControl.xaml
+    /// </summary>
+    public partial class ScheduleControl : UserControl
+    {
+        public ScheduleControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnVisualParentChanged(DependencyObject oldParent)
+        {
+            base.OnVisualParentChanged(oldParent);
+
+            if (this.Parent != null)
+            {
+                this.Width = double.NaN;
+                this.Height = double.NaN;
+            }
+        }
+    }
+}
