9481b89e210ab1da552dae9b4591c2d75a371465
egor <Sapronov.egor@gmail.com>
2013-12-15 13:20:17 +0700

    Add schedule shift vm

---
+++ Schedule.Logic/ScheduleShift.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+
+namespace Schedule.Logic
+{
+    public class ScheduleShift : INotifyPropertyChanged
+    {
+        private string _name = string.Empty;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == null)
+                {
+                    value = string.Empty;
+                }
+                if (value != _name)
+                {
+                    _name = value;
+                    RaisePropertyChanged(new PropertyChangedEventArgs("Name"));
+
+                }
+            }
+        }
+
+        public void Copy(ScheduleShift scheduleShift)
+        {
+            Name = scheduleShift.Name;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, e);
+            }
+        }
+    }
+}
